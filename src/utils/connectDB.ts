c2739f8759ba5de43962524415bb46efc4b49b6c
annahico <annahico@gmail.com>
2024-08-25 19:38:10 +0200

    trying to solve dropall run error

--- src/utils/connectDB.ts
+++ src/utils/connectDB.ts
@@ -1,18 +1,18 @@
-import 'dotenv/config'
-import path from 'path'
-import { Sequelize } from 'sequelize'
-import { fileURLToPath } from 'url'
+import 'dotenv/config';
+import path from 'path';
+import { Sequelize } from 'sequelize';
+import { fileURLToPath } from 'url';
 
-// Obtener el nombre y el directorio del archivo actual
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// Definir __filename y __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Leer el archivo .env desde la ruta especificada
-const envPath = path.join(__dirname, '..', '.env')
+const envPath = path.join(__dirname, '..', '.env');
 
 // Asegurarse de que las variables de entorno están cargadas correctamente
 if (process.env.NODE_ENV !== 'production') {
-  import('dotenv').then((dotenv) => dotenv.config({ path: envPath }))
+  import('dotenv').then((dotenv) => dotenv.config({ path: envPath }));
 }
 
 // Configurar Sequelize con los parámetros desde las variables de entorno
@@ -28,23 +28,23 @@ const sequelize = new Sequelize(
       min: 10,
       max: 50,
       acquire: 30000,
-      idle: 10000
+      idle: 10000,
     },
     dialectOptions: {
       decimalNumbers: true,
-      maxPreparedStatements: 100
-    }
+      maxPreparedStatements: 100,
+    },
   }
-)
+);
 
 // Manejar la conexión y las posibles excepciones
 sequelize.authenticate()
   .then(() => {
-    console.log('Database connection established successfully.')
+    console.log('Database connection established successfully.');
   })
   .catch((error) => {
-    console.error('Unable to connect to the database:', error)
-    process.exit(1)
-  })
+    console.error('Unable to connect to the database:', error);
+    process.exit(1);
+  });
 
-export default sequelize
+export default sequelize;
